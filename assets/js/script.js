cd33c4063b76a297f24fc39c4d908b0866f0eca7
Nizar <fadlannizar5@gmail.com>
2020-12-13 00:08:56 +0700

    Initial empty commit

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+$(window).scroll(function() {
+   var sticky = $('nav'),
+      scroll = $(window).scrollTop();
+      
+   if (scroll > 0) { 
+      sticky.addClass('sticky');
+   }else { 
+      sticky.removeClass('sticky');
+   }
+});
+
+$('.navbar-toggler').on('click', function () {
+   $('.animated-icon').toggleClass('open')
+   if ($('.navbar-collapse').hasClass('show')){
+      $('body').attr('style', 'overflow: auto !important');
+   } else {
+      $('body').attr('style', 'overflow: hidden');
+   }
+});
+
+$('.btn-video').on('click', function () {
+   $('.materi-video').attr('style','width: 100% !important');
+   $('.btn-video').attr('style', 'display: none');
+});
+
+$('#video').on('ended', function() {
+   $('.btn-video').attr('style', 'display: block !important');
+});
