b4d810d103051b6a22d9ff5446629744d7f6a868
Nizar <40895148+nizariyf@users.noreply.github.com>
2020-12-13 13:37:20 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,13 +11,19 @@ $(window).scroll(function() {
 
 $('.navbar-toggler').on('click', function () {
    $('.animated-icon').toggleClass('open');
+   if ($('.navbar-collapse').hasClass('show')){
+      $('body').removeClass('snone');
+   } else {
+      $('body').addClass('snone');
+   }
 });
 
 $('.btn-video').on('click', function () {
-   $('.materi-video').attr('style','width: 100% !important');
-   $('.btn-video').attr('style', 'display: none');
+   $('.btn-video').addClass('btnhide');
+   $('.materi-video').addClass('fullvid');
 });
 
 $('#video').on('ended', function() {
-   $('.btn-video').attr('style', 'display: block !important');
+   $('.btn-video').removeClass('btnhide');
+   $('.materi-video').removeClass('fullvid');
 });
